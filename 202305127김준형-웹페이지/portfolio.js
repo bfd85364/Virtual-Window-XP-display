879164d40c8923732059bf8e6e6528916abbff29
202305217 김준형(junhyung-kim) <85354585+bfd85364@users.noreply.github.com>
2025-03-17 15:42:52 +0900

    Add files via upload

---
+++ 202305127김준형-웹페이지/portfolio.js
@@ -0,0 +1,48 @@
+function File_icon1(){
+	//javascript, css, html을 활용해서 웹페이지를 제작하며 프론트 엔드영역을 공부한 내용 적기  
+	var image = document.getElementById('img1');
+	var source = img1.src;
+	window.open(source);
+}
+
+function File_icon2(){
+//snake game폴더의 경로를 확인하시고, 저장위치가 달라질 경우 올바른 경로의 링크로 대체 해주시면 게임 플레이가 가능해지십니다. 
+//==만일 게임이 작동이 안되신다면 snake game 폴더에서 snake.html 파일 클릭해서 실행하고 해당 페이지 링크 ctrl C + V 으로 링크 교체
+//아이콘 클릭시 게임파일 실행-> 1~3중 원하는 레벨 입력 게임레벨 1~3단계까지 설정 가능 
+location.href='file:///C:/Users/User/Desktop/202305127%EA%B9%80%EC%A4%80%ED%98%95-%EC%9B%B9%ED%8E%98%EC%9D%B4%EC%A7%80/Snake%20game/Snake.html';
+}
+
+function File_icon3(){
+	// python을 활용하여 진행하였던 프로젝트에 대해 활용 -> 데이터 분석, 머신러닝 모델 활용, 머신러닝 디스코드 API 서버 검열봇 개발 진행한거 
+	var image = document.getElementById('img3');
+	var source = img3.src;
+	window.open(source);
+}
+
+function File_icon4() { 
+	//java, c++ 등 oop기법에 대해 배웠어요. 
+	var image = document.getElementById('img4');
+	var source = img4.src;
+	window.open(source);
+}
+			
+function File_icon5() {
+	var image = document.getElementById('img5');
+	var source = img5.src;
+	window.open(source);
+}
+
+
+function File_icon6(){
+	//포토폴리오용 깃허브 링크 걸기 
+	var image = document.getElementById('img6');
+	location.href="https://github.com/bfd85364";
+}
+
+function File_icon7(){
+	//휴지통 파일에서는 실패한 프로젝트 언급 -> 다익스트라 알고리즘을 활용한 길찾기 로봇 만들기
+	//-> 초기형 레고 마인드 스톰 NXT 모델의 비주얼 프로그래밍으로 다익스트라 알고리즘을 재현하여 길찾기 로봇을 만들려다가 정보부족으로 실패함 
+	var image = document.getElementById('img7');
+	var source = img7.src;
+	window.open(source);
+}
