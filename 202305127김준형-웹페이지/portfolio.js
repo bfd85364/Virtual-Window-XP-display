992b80e33c1d620a0c84b5ab56cd72715cf0f1e5
202305217 김준형(junhyung-kim) <85354585+bfd85364@users.noreply.github.com>
2025-05-14 14:02:59 +0900

    Add files via upload

--- 202305127김준형-웹페이지/portfolio.js
+++ 202305127김준형-웹페이지/portfolio.js
@@ -1,3 +1,9 @@
+function Start_button(){
+	
+	
+	
+}
+
 function File_icon1(){
 	//javascript, css, html을 활용해서 웹페이지를 제작하며 프론트 엔드영역을 공부한 내용 적기  
 	var image = document.getElementById('img1');
@@ -7,7 +13,7 @@ function File_icon1(){
 
 function File_icon2(){
 //snake game폴더의 경로를 확인하시고, 저장위치가 달라질 경우 올바른 경로의 링크로 대체 해주시면 게임 플레이가 가능해지십니다. 
-//==만일 게임이 작동이 안되신다면 snake game 폴더에서 snake.html 파일 클릭해서 실행하고 해당 페이지 링크 ctrl C + V 으로 링크 교체
+//==만일 게임이 작동이 안되신다면 snake game 폴더에서 snake.html 파일 클릭해서 실행하고 해당 페이지의 웹페이지 링크 ctrl C + V 으로 복사후후 링크 교체
 //아이콘 클릭시 게임파일 실행-> 1~3중 원하는 레벨 입력 게임레벨 1~3단계까지 설정 가능 
 location.href='file:///C:/Users/User/Desktop/202305127%EA%B9%80%EC%A4%80%ED%98%95-%EC%9B%B9%ED%8E%98%EC%9D%B4%EC%A7%80/Snake%20game/Snake.html';
 }
